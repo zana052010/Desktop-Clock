48cd8bb362a0e7796ffd3b1715974a1444e1a537
Zainutdin <d9317680@gmail.com>
2026-08-22 21:11:18 +0300

    First commit

---
+++ ClockOnDesk/Class1.cs
@@ -0,0 +1,8 @@
+
+namespace ClockOnDesk
+{
+    public class Class1
+    {
+    }
+
+}
---
+++ ClockOnDesk/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClockOnDesk
+{
+    public partial class MainWindow : Window
+    {
+
+        private DispatcherTimer _timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick_For_Date;
+            _timer.Tick += Timer_Tick_For_Days;
+            _timer.Start();
+        }
+
+        private void Timer_Tick_For_Date(object sender, EventArgs e)
+        {
+
+            if (Time != null)
+            {
+                Time.Text = DateTime.Now.ToString("HH:mm");
+            }
+        }
+        private void Timer_Tick_For_Days(object sender, EventArgs e)
+        {
+            if (Time != null)
+            {
+                Date.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            }
+        }
+    }
+}
